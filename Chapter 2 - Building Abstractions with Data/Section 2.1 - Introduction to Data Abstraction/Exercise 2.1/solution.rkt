cc7ceb4aeacb5aaa8b48a89f9ef33b259c29f951
Abhiram Reddy <nrabhiramr@gmail.com>
2023-11-10 14:29:58 +0530

    2.1.1 exercise set

---
+++ Chapter 2 - Building Abstractions with Data/Section 2.1 - Introduction to Data Abstraction/Exercise 2.1/solution.rkt
@@ -0,0 +1,52 @@
+#lang sicp
+
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
+(define (make-rat n d)
+  (let ((g (gcd (abs n) (abs d)))
+        (normalizer
+         (if (< d 0) -1 1)))
+    (cons (* normalizer
+             (/ n g))
+          (* normalizer
+             (/ d g)))))
+
+(define (numer x) (car x))
+
+(define (denom x) (cdr x))
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(print-rat (make-rat -2 4))
+(print-rat (make-rat 3 9))
+(print-rat (sub-rat (make-rat 1 3)
+                    (make-rat 1 2)))
